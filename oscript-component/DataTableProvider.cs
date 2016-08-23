f42cb9ab640a8fa692cb9d0b3829bc21c16a6bcd
ret-Phoenix <phoenixusa@yandex.ru>
2016-08-23 23:19:01 +0300

    start datagridview

---
+++ oscript-component/DataTableProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.HostedScript.Library.ValueTable;
+using System.Data;
+
+namespace oscriptGUI
+{
+    [ContextClass("Провайдер", "Provider")]
+    class DataTableProvider : AutoContext<DataTableProvider>
+    {
+        private ValueTable _valueTable;
+        private DataTable _dataTable;
+
+        public DataTableProvider()
+        {
+            _valueTable = new ValueTable();
+        }
+
+        private void setProviderValueTable()
+        {
+            _dataTable.Clear();
+
+            
+
+            //foreach (ValueTableColumn VTCol in _valueTable.Columns)
+            //{
+            //    _dataTable.Columns.Add(VTCol.Name);
+            //}
+        }
+
+        [ContextProperty("Источник", "Source")]
+        public ValueTable Height
+        {
+            get { return _valueTable; }
+            set { _valueTable = value; setProviderValueTable(); }
+        }
+    }
+}
---
+++ oscript-component/FormTable.cs
@@ -0,0 +1,239 @@
+﻿using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace oscriptGUI
+{
+    [ContextClass("ТаблицаФормы", "FormTable")]
+    class FormTable : AutoContext<FormTable>, IFormElement
+    {
+        private Control _item;
+        private Control _parentControl;
+        private Panel _panel;
+
+        private string _name;
+        private bool _visible;
+        private bool _enabled;
+        private string _title;
+        //private string _toolTip;
+        private IElementsContainer _parent;
+        private IRuntimeContextInstance _thisScript;
+        private string _methodName;
+
+        private BindingSource _bindingSource = new BindingSource();
+        private DataTable _dataTable ;
+
+        public FormTable(Control parentCntrl)
+        {
+            this._item = new DataGridView();
+//            ((Button)this._item).Click += BtnClick;
+
+            this._item.MinimumSize = new Size(100, 21);
+            this._item.AutoSize = true;
+
+            this._parentControl = parentCntrl;
+            this._panel = new Panel();
+
+            this._name = "";
+            this._visible = true;
+            this._enabled = true;
+            this._title = "";
+            this._parent = null;
+            this._methodName = "";
+            this._thisScript = null;
+
+            this._panel.Dock = DockStyle.Top;
+            this._panel.AutoSize = true;
+            this._panel.Controls.Add(this._item);
+            this._parentControl.Controls.Add(_panel);
+            this._panel.BringToFront();
+        }
+
+        public override string ToString()
+        {
+            return "ТаблицаФормы";
+        }
+
+        public Control getBaseControl()
+        {
+            return _panel;
+        }
+
+        public Control getControl()
+        {
+            return _item;
+        }
+
+        public void setParent(IValue parent)
+        {
+            _parent = (IElementsContainer)parent;
+        }
+
+        /// <summary>
+        /// Содержит ссылку на родительский элемент. <see cref="FormGroup"/>
+        /// </summary>
+        /// <value>ГруппаФормы, Форма</value>
+        [ContextProperty("Родитель", "Parent")]
+        public IValue Parent
+        {
+            get { return this._parent; }
+        }
+
+
+        /// <summary>
+        /// Уникальное имя элемента
+        /// </summary>
+        [ContextProperty("Имя", "Name")]
+        public string Name
+        {
+            get { return this._name; }
+            set
+            {
+
+                this._parent.Items.renameElement(this._name, value);
+                this._name = value;
+            }
+        }
+
+        /// <summary>
+        /// Управление видимостью кнопки
+        /// </summary>
+        [ContextProperty("Видимость", "Visible")]
+        public bool Visible
+        {
+            get { return this._visible; }
+            set
+            {
+                this._visible = value;
+                this._panel.Visible = value;
+            }
+        }
+
+        /// <summary>
+        /// Управление доступностью
+        /// </summary>
+        [ContextProperty("Доступность", "Enabled")]
+        public bool Enabled
+        {
+            get { return this._enabled; }
+            set
+            {
+                this._enabled = value;
+                this._panel.Enabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Надпись на кнопке
+        /// </summary>
+        [ContextProperty("Заголовок", "Title")]
+        public string Title
+        {
+            get { return this._title; }
+            set
+            {
+                this._title = value;
+                ((Button)this._item).Text = this._title;
+            }
+        }
+
+        /// <summary>
+        /// Высота кнопки
+        /// </summary>
+        [ContextProperty("Высота", "Height")]
+        public int Height
+        {
+            get { return _item.Height; }
+            set { _item.Height = value; }
+        }
+
+        /// <summary>
+        /// Ширина кнопки
+        /// </summary>
+        [ContextProperty("Ширина", "Width")]
+        public int Width
+        {
+            get { return _item.Width; }
+            set { _item.Width = value; }
+        }
+
+        /// <summary>
+        /// Автоматический размер
+        /// </summary>
+        [ContextProperty("АвтоматическийРазмер", "AutoSize")]
+        public bool AutoSize
+        {
+            get { return _item.AutoSize; }
+            set { _item.AutoSize = value; }
+        }
+
+        /// <summary>
+        /// Вариант закрепления. <see cref="FormControlDockStyle"/>
+        /// </summary>
+        /// <value>СтильЗакрепления</value>
+        [ContextProperty("Закрепление", "Dock")]
+        public int Dock
+        {
+            get { return _item.Dock.GetHashCode(); }
+            set
+            {
+                _item.Dock = (DockStyle)value;
+            }
+        }
+
+
+        [ContextProperty("ПутьКДанным", "DataPath")]
+        public DataTable DataPath
+        {
+            get { return _dataTable; }
+            set
+            {
+                _dataTable = value;
+            }
+        }
+
+        /// <summary>
+        /// Установить обработчик события.
+        /// Возможные события:
+        /// - Нажатие - Нажатие на кнопку
+        /// </summary>
+        /// <param name="contex">Ссылка на скрипт в котором находится обработчик события</param>
+        /// <param name="eventName">Имя обрабатываемого события</param>
+        /// <param name="methodName">Имя метода обработчика события</param>
+        /// <example>
+        ///  Форма.УстановитьДействие(ЭтотОбъект, "ПриОткрытии", "ПриОткрытииФормы");
+        /// </example>
+        [ContextMethod("УстановитьДействие", "SetAction")]
+        public void setAction(IRuntimeContextInstance contex, string eventName, string methodName)
+        {
+            if (eventName == "Нажатие")
+            {
+                //((Button)this._item).Click -= BtnClick;
+                //((Button)this._item).Click += BtnClick;
+                this._thisScript = contex;
+                this._methodName = methodName;
+            }
+        }
+
+        /// <summary>
+        /// Получает имя установленного обработчика события.
+        /// </summary>
+        /// <param name="eventName">Имя события</param>
+        /// <returns>Имя метода обработчика события</returns>
+        /// <example>
+        /// Форма.УстановитьДействие(ЭтотОбъект, "ПриОткрытии", "ПриОткрытииФормы");
+        /// Форма.ПолучитьДействие("ПриОткрытии");
+        /// // вернет: "ПриОткрытииФормы"
+        /// </example>
+        [ContextMethod("ПолучитьДействие", "GetAction")]
+        public string GetAction(string eventName)
+        {
+            return "" + ((ScriptInformationContext)this._thisScript).Source + ":" + this._methodName;
+        }
+
+        
+    }
+}
