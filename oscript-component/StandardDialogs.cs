8eb842128a2bcb5b05bd11b942798b4f4774510d
alexkmbk <alexkmbk@gmail.com>
2019-01-12 01:18:47 +0600

    Реализованы функции ВвестиСтроку\InputString и ВвестиЧисло\InputNumber

--- oscript-component/StandardDialogs.cs
+++ oscript-component/StandardDialogs.cs
@@ -11,6 +11,7 @@ using System;
 using ScriptEngine.Machine;
 using System.Drawing;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace oscriptGUI
 {
@@ -55,6 +56,58 @@ namespace oscriptGUI
         //{
         //    return new StandardDialogs();
         //}
+
+        /// <summary>
+        /// 
+        /// Вызывает диалог для ввода числа.
+        /// </summary>
+        ///
+        /// <param name="Number">
+        /// Имя доступной в модуле переменной. В эту переменную будет помещено введенное число. Начальное значение переменной будет использовано в качестве начального значения в диалоге. </param>
+        /// <param name="Tooltip">
+        /// Текст заголовка окна диалога ввода числа. Может использоваться в качестве подсказки пользователю.
+        /// Значение по умолчанию: Пустая строка. </param>
+        /// <param name="Length">
+        /// Длина вводимого числа включая дробную часть (символы разделителей не учитываются).
+        /// Значение по умолчанию: 0. </param>
+        /// <param name="Precision">
+        /// Количество знаков в дробной части вводимого числа.
+        /// Значение по умолчанию: 0. </param>
+        ///
+        /// <returns name="Boolean">
+        /// Значения данного типа имеют два значения Истина и Ложь, задаваемых соответствующими литералами. 
+        /// Значения данного типа возвращаются в качестве результата вычисления логических выражений.</returns>
+        ///
+        [ContextMethod("ВвестиЧисло", "InputNumber")]
+        public bool InputNumber([ByRef] IVariable Number, string Tooltip = null, int Length = 0, int Precision = 0)
+        {
+            return InputNumberDialog.Show(Number, Tooltip, Length, Precision);
+        }
+
+        /// <summary>
+        /// Вызывает диалог для ввода строки.
+        /// </summary>
+        ///
+        /// <param name="String">
+        /// Доступная в модуле переменная. В эту переменную будет помещена введенная в диалоге строка. Начальное значение переменной будет использовано в качестве начального значения в диалоге. </param>
+        /// <param name="Tooltip">
+        /// Текст заголовка окна диалога ввода строки. Может использоваться в качестве подсказки пользователю.
+        /// Значение по умолчанию: Пустая строка. </param>
+        /// <param name="Length">
+        /// Длина вводимой строки. Если параметр не указан, то строка неограниченной длины.
+        /// Значение по умолчанию: 0. </param>
+        /// <param name="Multiline">
+        /// Определяет режим ввода многострочного текста: Истина - ввод многострочного текста с разделителями строк; Ложь - ввод простой строки.
+        /// Значение по умолчанию: Ложь. </param>
+        /// <returns name="Boolean">
+        /// Значения данного типа имеют два значения Истина и Ложь, задаваемых соответствующими литералами. 
+        /// Значения данного типа возвращаются в качестве результата вычисления логических выражений.</returns>
+        ///
+        [ContextMethod("ВвестиСтроку", "InputString")]
+        public bool InputString([ByRef] IVariable ValueString, string Tooltip = null, int Length = 0, bool Multiline = false)
+        {
+            return InputStringDialog.Show(ValueString, Tooltip, Length, Multiline);
+        }
 
     }
 
@@ -360,4 +413,404 @@ namespace oscriptGUI
 
     }
 
+    // Класс для отображения окна диалога для ввода номера
+    //
+    public class InputNumberDialog
+    {
+        Form _form;
+        Button _okButton;
+        Button _cancelButton;
+        TextBox _ValueField;
+        Label _Label;
+        bool isOK;
+        decimal ResultNumber;
+        char NumberDecimalSeparator;
+
+        InputNumberDialog(string Number, string Tooltip = null, int Length = 0, int Precision = 0)
+        {
+
+            // Gets a NumberFormatInfo associated with the en-US culture.
+            NumberFormatInfo nfi = CultureInfo.CurrentCulture.NumberFormat;
+
+            // Displays the same value with a blank as the separator.
+            NumberDecimalSeparator = nfi.NumberDecimalSeparator[0];
+
+            isOK = false;
+
+            int formfWidth = 300;
+
+            _form = new Form();
+
+            _form.AutoSize = false;
+            _form.Height = 200;
+            if (Tooltip == null || Tooltip.Length == 0)
+                _form.Text = "Please enter a number";
+            else
+                _form.Text = Tooltip;
+            _form.StartPosition = FormStartPosition.CenterScreen;
+            _form.MaximizeBox = false;
+            _form.MinimizeBox = false;
+            _form.FormBorderStyle = FormBorderStyle.FixedDialog;
+
+            // Label
+            _Label = new Label();
+            _Label.Text = _form.Text;
+            _Label.Padding = new Padding(20, 20, 20, 20);
+            _Label.TextAlign = ContentAlignment.MiddleCenter;
+            _Label.AutoSize = true;
+            _Label.Font = new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold);
+
+            using (Graphics g = _form.CreateGraphics())
+            {
+                SizeF size = g.MeasureString(_Label.Text, _Label.Font);
+                formfWidth = Math.Max((int)Math.Ceiling(size.Width) + _Label.Padding.Horizontal, formfWidth);
+                formfWidth = Math.Min(formfWidth, 600);
+            }
+
+            _form.Width = formfWidth;
+            _form.MinimumSize = new Size(formfWidth, 200);
+
+            TableLayoutPanel CommonPanel = new TableLayoutPanel();
+            CommonPanel.ColumnCount = 1;
+            CommonPanel.RowCount = 3;
+            CommonPanel.AutoSize = true;
+            _form.Controls.Add(CommonPanel);
+
+            Panel panel = new Panel();
+            panel.Dock = DockStyle.Top;
+            panel.AutoSize = true;
+            panel.Controls.Add(_Label);
+            CommonPanel.Controls.Add(panel);
+            panel.BringToFront();
+
+            // OK Button
+            _okButton = new Button();
+            _okButton.Click += OKClick;
+            _okButton.Text = "OK";
+            _okButton.MinimumSize = new Size(100, 21);
+            _okButton.AutoSize = true;
+            _okButton.Anchor = AnchorStyles.None;
+            _okButton.Left = (_form.Width - _okButton.Width * 2) / 2;
+            _okButton.Margin = new Padding(10, 20, 10, 20);
+
+            // Cancel Button
+            _cancelButton = new Button();
+            _cancelButton.Click += CancelClick;
+            _cancelButton.Text = "Cancel";
+            _cancelButton.MinimumSize = new Size(100, 21);
+            _cancelButton.AutoSize = true;
+            _cancelButton.Anchor = AnchorStyles.None;
+            _cancelButton.Left = _okButton.Left + _cancelButton.Width + 5;
+            _cancelButton.Margin = new Padding(10, 20, 10, 20);
+
+            // Text field
+
+            _ValueField = new TextBox();
+            _ValueField.Text = Number;
+            _ValueField.Width = formfWidth - 20;
+            _ValueField.KeyPress += ValueField_KeyPress;
+
+            panel = new Panel();
+            panel.Height = 20;
+            panel.Width = formfWidth;
+            panel.Dock = DockStyle.Bottom;
+            panel.AutoSize = true;
+            panel.Controls.Add(_ValueField);
+            panel.BringToFront();
+            CommonPanel.Controls.Add(panel);
+
+
+            panel = new Panel();
+            panel.Height = 20;
+            panel.Width = formfWidth;
+            panel.Dock = DockStyle.Bottom;
+            panel.AutoSize = true;
+            panel.Controls.Add(_okButton);
+            panel.Controls.Add(_cancelButton);
+            panel.BringToFront();
+            CommonPanel.Controls.Add(panel);
+
+
+            _form.ShowDialog(null);
+        }
+
+        public static bool Show(IVariable Value, string Tooltip = null, int Length = 0, int Precision = 0)
+        {
+            InputNumberDialog dlg = new InputNumberDialog(Value.Value.AsString(), Tooltip, Length, Precision);
+            Value.Value = ValueFactory.Create(dlg.ResultNumber);
+            return dlg.isOK;
+        }
+
+        public void OKClick(object sender, EventArgs e)
+        {
+            isOK = true;
+            ResultNumber = Convert.ToDecimal(_ValueField.Text);
+            _form.Close();
+        }
+        public void CancelClick(object sender, EventArgs e)
+        {
+            isOK = false;
+            _form.Close();
+        }
+
+        private void ValueField_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !IsOKForDecimalTextBox(e.KeyChar, (TextBox)sender);
+        }
+
+        private bool IsOKForDecimalTextBox(char theCharacter, TextBox theTextBox)
+        {
+            // Only allow control characters, digits, plus and minus signs.
+            // Only allow ONE plus sign.
+            // Only allow ONE minus sign.
+            // Only allow the plus or minus sign as the FIRST character.
+            // Only allow ONE decimal point.
+            // Do NOT allow decimal point or digits BEFORE any plus or minus sign.
+
+            if (
+                !char.IsControl(theCharacter)
+                && !char.IsDigit(theCharacter)
+                && (theCharacter != NumberDecimalSeparator)
+                && (theCharacter != ',')
+                && (theCharacter != '-')
+                && (theCharacter != '+')
+            )
+            {
+                // Then it is NOT a character we want allowed in the text box.
+                return false;
+            }
+
+
+
+            // Only allow one decimal point.
+            if (theCharacter == NumberDecimalSeparator
+                && theTextBox.Text.IndexOf(NumberDecimalSeparator) > -1)
+            {
+                // Then there is already a decimal point in the text box.
+                return false;
+            }
+
+            // Only allow one minus sign.
+            if (theCharacter == '-'
+                && theTextBox.Text.IndexOf('-') > -1)
+            {
+                // Then there is already a minus sign in the text box.
+                return false;
+            }
+
+            // Only allow one plus sign.
+            if (theCharacter == '+'
+                && theTextBox.Text.IndexOf('+') > -1)
+            {
+                // Then there is already a plus sign in the text box.
+                return false;
+            }
+
+            // Only allow one plus sign OR minus sign, but not both.
+            if (
+                (
+                    (theCharacter == '-')
+                    || (theCharacter == '+')
+                )
+                &&
+                (
+                    (theTextBox.Text.IndexOf('-') > -1)
+                    ||
+                    (theTextBox.Text.IndexOf('+') > -1)
+                )
+                )
+            {
+                // Then the user is trying to enter a plus or minus sign and
+                // there is ALREADY a plus or minus sign in the text box.
+                return false;
+            }
+
+            // Only allow a minus or plus sign at the first character position.
+            if (
+                (
+                    (theCharacter == '-')
+                    || (theCharacter == '+')
+                )
+                && theTextBox.SelectionStart != 0
+                )
+            {
+                // Then the user is trying to enter a minus or plus sign at some position 
+                // OTHER than the first character position in the text box.
+                return false;
+            }
+
+            // Only allow digits and decimal point AFTER any existing plus or minus sign
+            if (
+                    (
+                        // Is digit or decimal point
+                        char.IsDigit(theCharacter)
+                        ||
+                        (theCharacter == NumberDecimalSeparator)
+                    )
+                    &&
+                    (
+                        // A plus or minus sign EXISTS
+                        (theTextBox.Text.IndexOf('-') > -1)
+                        ||
+                        (theTextBox.Text.IndexOf('+') > -1)
+                    )
+                    &&
+                        // Attempting to put the character at the beginning of the field.
+                        theTextBox.SelectionStart == 0
+                )
+            {
+                // Then the user is trying to enter a digit or decimal point in front of a minus or plus sign.
+                return false;
+            }
+
+            // Otherwise the character is perfectly fine for a decimal value and the character
+            // may indeed be placed at the current insertion position.
+            return true;
+        }
+    }
+
+
+    // Класс для отображения окна диалога для ввода номера
+    //
+    public class InputStringDialog
+    {
+        Form _form;
+        Button _okButton;
+        Button _cancelButton;
+        TextBox _ValueField;
+        Label _Label;
+        bool isOK;
+        string ResultString;
+
+        InputStringDialog(string String, string Tooltip = null, int Length = 0, bool Multiline = false)
+        {
+            isOK = false;
+            ResultString = String;
+
+            int formfWidth = 300;
+            int formfHeight = 200;
+
+            if (Multiline)
+                formfHeight = 300;
+
+            _form = new Form();
+
+            _form.AutoSize = false;
+            _form.Height = formfHeight;
+            if (Tooltip == null || Tooltip.Length == 0)
+                _form.Text = "Please enter a string";
+            else
+                _form.Text = Tooltip;
+            _form.StartPosition = FormStartPosition.CenterScreen;
+            _form.MaximizeBox = false;
+            _form.MinimizeBox = false;
+
+            // Label
+            _Label = new Label();
+            _Label.Text = _form.Text;
+            _Label.Padding = new Padding(20, 20, 20, 20);
+            _Label.TextAlign = ContentAlignment.MiddleCenter;
+            _Label.AutoSize = true;
+            _Label.Font = new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold);
+
+            using (Graphics g = _form.CreateGraphics())
+            {
+                SizeF size = g.MeasureString(_Label.Text, _Label.Font);
+                formfWidth = Math.Max((int)Math.Ceiling(size.Width) + _Label.Padding.Horizontal, formfWidth);
+                formfWidth = Math.Min(formfWidth, 600);
+            }
+
+            _form.Width = formfWidth;
+            _form.MinimumSize = new Size(formfWidth, formfHeight);
+
+            TableLayoutPanel CommonPanel = new TableLayoutPanel();
+            CommonPanel.ColumnCount = 1;
+            CommonPanel.RowCount = 3;
+            CommonPanel.AutoSize = true;
+            _form.Controls.Add(CommonPanel);
+
+            Panel panel = new Panel();
+            panel.Dock = DockStyle.Top;
+            panel.AutoSize = true;
+            panel.Controls.Add(_Label);
+            CommonPanel.Controls.Add(panel);
+            panel.BringToFront();
+
+            // OK Button
+            _okButton = new Button();
+            _okButton.Click += OKClick;
+            _okButton.Text = "OK";
+            _okButton.MinimumSize = new Size(100, 21);
+            _okButton.AutoSize = true;
+            _okButton.Anchor = AnchorStyles.None;
+            _okButton.Left = (_form.Width - _okButton.Width*2) / 2;
+            _okButton.Margin = new Padding(10, 20, 10, 20);
+
+            // Cancel Button
+            _cancelButton = new Button();
+            _cancelButton.Click += CancelClick;
+            _cancelButton.Text = "Cancel";
+            _cancelButton.MinimumSize = new Size(100, 21);
+            _cancelButton.AutoSize = true;
+            _cancelButton.Anchor = AnchorStyles.None;
+            _cancelButton.Left = _okButton.Left + _cancelButton.Width + 5;
+            _cancelButton.Margin = new Padding(10, 20, 10, 20);
+
+            // Text field
+
+            _ValueField = new TextBox();
+            if (Multiline)
+            {
+                _ValueField.Height = 100;
+                _ValueField.ScrollBars = ScrollBars.Vertical;
+            }
+            _ValueField.Text = String;
+            _ValueField.Width = formfWidth - 20;
+            _ValueField.Multiline = Multiline;
+            if (Length != 0)
+                _ValueField.MaxLength = Length;
+
+            panel = new Panel();
+            panel.Height = 20;
+            panel.Width = formfWidth;
+            panel.Dock = DockStyle.Bottom;
+            panel.AutoSize = true;
+            panel.Controls.Add(_ValueField);
+            panel.BringToFront();
+            CommonPanel.Controls.Add(panel);
+
+
+            panel = new Panel();
+            panel.Height = 20;
+            panel.Width = formfWidth;
+            panel.Dock = DockStyle.Bottom;
+            panel.AutoSize = true;
+            panel.Controls.Add(_okButton);
+            panel.Controls.Add(_cancelButton);
+            panel.BringToFront();
+            CommonPanel.Controls.Add(panel);
+
+
+            _form.ShowDialog(null);
+        }
+
+        public static bool Show(IVariable Value, string Tooltip = null, int Length = 0, bool Multiline = false)
+        {
+            InputStringDialog dlg = new InputStringDialog(Value.Value.AsString(), Tooltip, Length, Multiline);
+            Value.Value = ValueFactory.Create(dlg.ResultString);
+            return dlg.isOK;
+        }
+
+        public void OKClick(object sender, EventArgs e)
+        {
+            isOK = true;
+            ResultString = _ValueField.Text;
+            _form.Close();
+        }
+        public void CancelClick(object sender, EventArgs e)
+        {
+            isOK = false;
+            _form.Close();
+        }
+    }
 }
